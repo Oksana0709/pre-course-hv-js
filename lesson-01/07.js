fda3664a038621556ab19673ea5d58086596d825
Oksana0709 <o07091980@gmail.com>
2023-11-13 23:36:24 +0300

    [seventh task]

---
+++ lesson-01/07.js
@@ -0,0 +1,15 @@
+
+// Дано:
+
+// ​let iAm = 'o'
+// let krasavchik = "красавчик!";
+
+// //   Выведи в консоль «Я красавчик!».
+// // ❗ Обрати внимание: «Я» с заглавной.
+
+
+
+let iAm = 'я';
+let krasavchik = 'красавчик!'
+
+console.log(iAm.toUpperCase(), krasavchik)
