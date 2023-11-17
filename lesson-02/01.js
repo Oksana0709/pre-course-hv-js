518886d83d1d118e808dc0f5ad757143118f0dcf
Oksana0709 <o07091980@gmail.com>
2023-11-17 22:55:07 +0300

    [commit]

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,9 +1,11 @@
-// code
+// Переделай функцию на стрелочный синтаксис
+
 // function doArrow(value) {
 //     console.log(value);
 // }
 // doArrow('Передай 5')
 
-
-let doArrow = (value) => value;
-console.log(doArrow('Передай 5'));
+const doArrow = (value) => {
+    return console.log(value = 'Передай 5')
+}
+doArrow()
