3f39b72e62a2d5959d8fca37d765b30e899ccbc6
Oksana0709 <o07091980@gmail.com>
2023-11-18 12:06:32 +0300

    [ 3 ]

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,8 +1,8 @@
-// Передай такие числа, что бы у нас выдало надпись 'Something wrong!'
+
 
 const giveMeRightNumbers = (value1, value2, maximum) => {
   if (value1 === value2 || value1 <= maximum) {
-    console.log('"Success!"');
+    console.log("Success!");
   } else {
     console.log("Something wrong!");
   }
