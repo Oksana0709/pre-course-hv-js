2f7cb055d8b65056deefe4fe5bc5cb2a7f10732a
Oksana0709 <o07091980@gmail.com>
2023-11-20 12:48:49 +0300

    [task five]

---
+++ lesson-02/05.js
@@ -0,0 +1,19 @@
+
+//  Дан объект passport. Создай его копию и поменяй имя с "Petr" на "Ivan", и убедись 
+// в консоли, что у объекта passport имя осталось "Petr", а у твоего нового объекта "Ivan".
+// ❗Для того, чтобы проверить правильность данного задания должно быть выведено 2 console.log
+
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+}
+
+let passport2 = {...passport}
+
+passport2 = {
+    name: "Ivan",
+    surname: "Petrov",
+}
+
+console.log(passport)
+console.log(passport2)
