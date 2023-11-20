2e714f04b94326fda5f3b2d684c5cfa3aed38bec
Oksana0709 <o07091980@gmail.com>
2023-11-20 12:53:06 +0300

    [task five  ]

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -4,16 +4,16 @@
 // ❗Для того, чтобы проверить правильность данного задания должно быть выведено 2 console.log
 
 let passport = {
-    name: "Petr",
-    surname: "Petrov",
-}
+  name: "Petr",
+  surname: "Petrov",
+};
 
-let passport2 = {...passport}
+let passport2 = { ...passport };
 
 passport2 = {
-    name: "Ivan",
-    surname: "Petrov",
-}
+  name: "Ivan",
+  surname: "Petrov",
+};
 
-console.log(passport)
-console.log(passport2)
+console.log(passport);
+console.log(passport2);
