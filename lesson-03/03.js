d80c32d81c37bea4f3f5d0a976b513bad8c6c8bb
Oksana0709 <o07091980@gmail.com>
2023-11-27 19:39:05 +0300

    [task 3]

---
+++ lesson-03/03.js
@@ -0,0 +1,6 @@
+
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+let slisedFruits = fruits.slice(1, 8)
+
+console.log(slisedFruits)
+
