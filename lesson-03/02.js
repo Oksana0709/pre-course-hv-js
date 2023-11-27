b78f785ffd1a95a6da0c7d68138930b243761ac2
Oksana0709 <o07091980@gmail.com>
2023-11-27 19:14:26 +0300

    [task second]

---
+++ lesson-03/02.js
@@ -0,0 +1,7 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+const result = words.indexOf("апельсин");
+if (result == 2) {
+  console.log("Ура! нашел");
+} else {
+  console.log("Придется поискать в другом магазине…");
+}
